eec2ec897f2407de4a688a8ddabdbe0a37d18b8d
Nuno Job <nunojobpinto@gmail.com>
2013-05-21 13:36:54 +0100

    leakswhatevs

---
+++ thestateofthestack/leak-1.js
@@ -0,0 +1,9 @@
+var dnsd = require('dnsd')
+
+dnsd.createServer(function handle(req, res) {
+  res.end('1.2.3.4')
+}).listen(5354, '127.0.0.1')
+
+console.log('Server running on 5354')
+
+// dig @localhost -p 5354 foo.example A
---
+++ thestateofthestack/leak-3.js
@@ -0,0 +1,12 @@
+var dnsd = require('dnsd')
+var leaks = []
+
+function LeakingClass() {}
+
+dnsd.createServer(function handle(req, res) {
+  res.end('1.2.3.4')
+}).listen(5354, '127.0.0.1')
+
+console.log('Server running on 5354')
+
+// dig @localhost -p 5354 foo.example A
